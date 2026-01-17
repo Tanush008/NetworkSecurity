0d9bfadda15cbabfa6578db78bb86543bdc8b522
*Tanush008* <*agggarwal.tanush1208@gmail.com*>
2026-01-18 01:39:14 +0530

    Implement data validation and transformation components with configuration updates

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_Transformation import DataTransformation
 import sys
 
 if __name__=="__main__":
@@ -12,8 +14,20 @@ if __name__=="__main__":
         data_ingestion = DataIngestion(dataingestionconfig)
         logging.info("Intiate the data ingestion")
         dataingestionartifact = data_ingestion.intiate_data_ingestion()
+        print("data ingestion is complete")
         print(dataingestionartifact)
-        
+        data_validation_config = DataValidationConfig(trainingPipelineConfig)
+        data_validation = DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Intiate the data validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Data validation is complete")
+        print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(trainingPipelineConfig)
+        logging.info("data Transformation started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data Transformation completed")
         
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -12,6 +12,7 @@ FILE_NAME:str = "phisingData.csv"
 TRAIN_FILE_NAME:str = "train.csv"
 TEST_FILE_NAME:str = "test.csv"
 
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
 
 """"data ingestion related const start with Data ingestion name"""
 
@@ -21,3 +22,27 @@ DATA_INGESTION_DIR_NAME :str = "data_ingestion"
 DATA_INGESTION_FEATURES_DIR:str = "feature_store"
 DATA_INGESTION_INGESTED_DIR :str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT:float = 0.2
+
+""" data validation related const start with data valid name"""
+DATA_VALIDATION_DIR_NAME :str= "data_validation"
+DATA_VALIDATION_VALID_DIR_NAME :str= "validation"
+DATA_VALIDATION_INVALID_DIR_NAME :str = "invalidation"
+DATA_VALIDATION_DRIFT_REPORT_DIR:str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_NAME :str = "report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+
+
+"""data trnsformation related const start with trans name"""
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+## kkn imputer to replace nan values
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
+    "missing_values": np.nan,
+    "n_neighbors": 3,
+    "weights": "uniform",
+}
+DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
+
+DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
